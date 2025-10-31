c085a1cecbd7ee7fa548717bffcd844b872e059b
christianAlbrand <christian.albrandaguirre@cesunbc.edu.mx>
2025-10-31 11:32:32 -0700

    added galleta

--- admin_core_TIENDA_GALLETAS/urls.py
+++ admin_core_TIENDA_GALLETAS/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('formulario/', formulario, name='formulario'),
     path('about/', about, name='about'),
     path('galleta/', galleta, name='galleta'),
+    path('galleta_create/', galleta_create, name='galleta_create'),
     path('orden/', orden, name='orden'),
     path('venta/', venta, name='venta'),
     path('detalle_venta/', detalle_venta, name='detalle_venta'),
