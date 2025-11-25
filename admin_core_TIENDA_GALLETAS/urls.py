64b411cd488682bba72cd0f8ad442cc08f6d5ca4
Esteban Torres <39715202+CHILIWILLY3240@users.noreply.github.com>
2025-11-24 19:58:54 -0800

    Cambios en templates y carrito

--- admin_core_TIENDA_GALLETAS/urls.py
+++ admin_core_TIENDA_GALLETAS/urls.py
@@ -10,5 +10,11 @@ urlpatterns = [
     path('orden/', orden, name='orden'),
     path('venta/', venta, name='venta'),
     path('detalle_venta/', detalle_venta, name='detalle_venta'),
-    
+    path('carrito/', carrito, name='carrito'),
+    path('carrito/agregar/<int:galleta_id>/', agregar_al_carrito, name='agregar_al_carrito'),
+    path('carrito/eliminar/<int:item_id>/', eliminar_del_carrito, name='eliminar_del_carrito'),
+    path('carrito/actualizar/<int:item_id>/', actualizar_cantidad, name='actualizar_cantidad'),
+    path('pago/', pago, name='pago'),
+    path('procesar-pago/', procesar_pago, name='procesar_pago'),
 ]
+
